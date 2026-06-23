27e19c6c5953d206ff5e68f4348a1171e94a6a2c
khbigyananda-art <khbigyananda@gmail.com>
2026-06-23 21:17:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const people = {
+
+    "bigyananda": {
+        fullname: "Bigyananda kh",
+        height: "5'9\"",
+        gender: "Sigma Male",
+        hobby: "Football CR7 SIUUUUUUUUU😈"
+    },
+
+    "tanishq": {
+        fullname: "Tunnik Tunnik Sholay",
+        height: "5'10\"",
+        gender: "Gay rainbow color patner Pabitra",
+        hobby: "Football Pessi fan"
+    },
+    "pabitra": {
+        fullname: "Gurumaahii Pabitra Shittt",
+        height: "6'2\"",
+        gender: "Gay patner Tanishq",
+        hobby: "Football Pessi fan"
+    },
+    "robertson": {
+        fullname: "Kh Ropertson Song",
+        height: "6'5\"",
+        gender: "Gay rainbow color patner Rohal",
+        hobby: "Sleeping"
+    },
+     "priya": {
+        fullname: "Priya Dhulai",
+        height: "4'5\"",
+        gender: "Mard ",
+        hobby: "thusna"
+    }
+};
+
+function searchPerson(){
+
+    const name =
+        document
+        .getElementById("nameInput")
+        .value
+        .toLowerCase();
+
+    const result =
+        document.getElementById("result");
+
+    if(people[name]){
+
+        result.innerHTML = `
+            <h3>${people[name].fullname}</h3>
+            <p>Height: ${people[name].height}</p>
+            <p>Gender: ${people[name].gender}</p>
+            <p>Hobby: ${people[name].hobby}</p>
+        `;
+
+    } else {
+
+        result.innerHTML =
+            "<p>No data found.</p>";
+
+    }
+
+}
